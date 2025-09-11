5086acff33743076660dface5c8b716e969a1034
Daniel Tolksdorf <daniel.tolksdorf@yahoo.de>
2025-09-11 14:43:46 +0200

    Bumbed new version

--- broadcaster/__init__.py
+++ broadcaster/__init__.py
@@ -1,5 +1,5 @@
 from ._base import Broadcast, Event
 from .backends.base import BroadcastBackend
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __all__ = ["Broadcast", "Event", "BroadcastBackend"]
